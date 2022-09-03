af624f5d6f888c3a66d4005e85490d8cffa5623c
janipang <pang.janista@gmail.com>
2022-09-04 03:54:01 +0700

    upload week5'

--- WEEKLY/week5/add_matrix_sol1.c
+++ WEEKLY/week5/add_matrix_sol1.c
@@ -2,32 +2,34 @@
 
 int main()
 {
-    int matrix1[2][3] , matrix2[2][3], matrix3[2][3] ;
-    for(int i = 0 ; i < 2 ; i++)
+    int m , n ;
+    scanf("%d %d" ,&m ,&n) ;
+    int matrix1[m][n] , matrix2[m][n], matrix3[m][n] ;
+    for(int i = 0 ; i < m ; i++)
     {
-        for (int j = 0 ; j < 3 ; j++)
+        for (int j = 0 ; j < n ; j++)
         {
             scanf("%d" ,&matrix1[i][j]) ;
         }
     }
-    for(int i = 0 ; i < 2 ; i++)
+    for(int i = 0 ; i < m ; i++)
     {
-        for (int j = 0 ; j < 3 ; j++)
+        for (int j = 0 ; j < n ; j++)
         {
             scanf("%d" ,&matrix2[i][j]) ;
         }
     }
-    for(int i = 0 ; i < 2 ; i++)
+    for(int i = 0 ; i < m ; i++)
     {
-        for (int j = 0 ; j < 3 ; j++)
+        for (int j = 0 ; j < n ; j++)
         {
             matrix3[i][j] =  matrix1[i][j] + matrix2[i][j];
         }
     }
-    for(int i = 0 ; i < 2 ; i++)
+    for(int i = 0 ; i < m ; i++)
     {
         printf("\n") ;
-        for (int j = 0 ; j < 3 ; j++)
+        for (int j = 0 ; j < n ; j++)
         {
             printf("%d " ,matrix3[i][j]);
         }
--- WEEKLY/week5/add_matrix_sol2.c
+++ WEEKLY/week5/add_matrix_sol2.c
@@ -2,17 +2,19 @@
 
 int main()
 {
-    int matrix1[2][3] , matrix2[2][3];
-    for(int i = 0 ; i < 2 ; i++)
+    int m , n ;
+    scanf("%d %d" ,&m ,&n) ;
+    int matrix1[m][n] , matrix2[m][n];
+    for(int i = 0 ; i < m ; i++)
     {
-        for (int j = 0 ; j < 3 ; j++)
+        for (int j = 0 ; j < n ; j++)
         {
             scanf("%d" ,&matrix1[i][j]) ;
         }
     }
-    for(int i = 0 ; i < 2 ; i++)
+    for(int i = 0 ; i < m ; i++)
     {
-        for (int j = 0 ; j < 3 ; j++)
+        for (int j = 0 ; j < n ; j++)
         {
             scanf("%d" ,&matrix2[i][j]) ;
             matrix2[i][j] = matrix1[i][j] + matrix2[i][j] ;
